bd6851f313ea0842a337a6b71b4c22c9ba72d3ed
Aditya Anand <69291902+redterror21@users.noreply.github.com>
2026-01-05 22:06:46 +0530

    commented

--- request_test.py
+++ request_test.py
@@ -5,13 +5,13 @@ URL = "https://www.basketball-reference.com/leagues/NBA_2026_games-november.html
 scraper = cloudscraper.create_scraper(
     browser={
         "browser": "chrome",
-        "platform": "windows",
+        "platform": "windows", #to fool the website and firewall into thinking this is a real user
         "mobile": False
     }
 )
 
 r = scraper.get(URL)
 
-print("Status Code:", r.status_code)
-print("HTML length:", len(r.text))
-print(r.text[:300])
+print("Status Code:", r.status_code) #200 is good 403 is fail
+print("HTML length:", len(r.text)) #should be 200000+
+print(r.text[:300]) # first 300 string chars of the webpage
